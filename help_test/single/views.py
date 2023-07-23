bff5061518b739b1850bb7a02b9253a9aa1bb5f1
Abstrakcionist <kondratev04g@mail.ru>
2023-07-23 09:07:47 +0500

    final

--- help_test/single/views.py
+++ help_test/single/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
-from .models import Inputs
 from .forms import InputsForm
+from .word import document_builder
 
 
 def index(request):
+    # создание массива с информацией из формы
+    user_information = []
     error = ''
     if request.method == 'POST':
         form = InputsForm(request.POST)
         if form.is_valid():
+            # сохранение в базу данных
             form.save()
+
+            # добавление данных из полей в массив
+            user_information.append(form.cleaned_data.get('username'))
+            user_information.append(form.cleaned_data.get('date_of_birth'))
+            user_information.append(form.cleaned_data.get('info'))
+
+            # запуск функции по созданию ворд документа
+            document_builder(user_information)
         else:
-            error = 'FY'
+            error = 'Что то не так'
 
     form = InputsForm()
 
     data = {
         'form': form,
-        'error': error
+        'error': error,
     }
     return render(request, 'single/index.html', data)
